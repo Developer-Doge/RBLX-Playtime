9611ad9158ee20ee4810b7ebe2d55cb6e7cef55f
RE4L1SM <RE4L1SM.games@gmail.com>
2021-12-27 11:17:18 -0500

    AGF Init

---
+++ src/Server/Services/TestService.lua
@@ -0,0 +1,20 @@
+-- Test Service
+-- Username
+-- December 27, 2021
+
+
+
+local TestService = {Client = {}}
+
+
+function TestService:Start()
+	
+end
+
+
+function TestService:Init()
+	
+end
+
+
+return TestService
